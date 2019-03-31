0bc0492c26f5c15adc49605a05c680dd4f22200e
jiangronghua <613870505@qq.com>
2019-04-01 00:19:40 +0800

    init

---
+++ src/components/function.js
@@ -0,0 +1,15 @@
+"use strict";
+import jQuery from 'jquery'
+!function(t, i) { 
+    var e = { id: "#cardArea", sid: ".card-item" };
+    i.fn.cardArea = function(t) { 
+        var t = i.extend({}, e, t);
+        return this.each(function() { 
+            var e = i(t.id),
+                n = e.find(t.sid);
+            n.on("mouseenter", function() {
+                i(this).addClass("active").siblings().removeClass("active") 
+            })
+        })
+    };
+}(window, jQuery);
